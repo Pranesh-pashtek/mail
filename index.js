8c4e037296720ca8364f6695049ca4aa2c5ee1f6
Pranesh-pashtek <117627550+Pranesh-pashtek@users.noreply.github.com>
2023-03-17 17:10:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/form", (req, res) => {
+  console.log(req.body);
+  //Formating content to be send
+  var emailcontent = `<h3> Contact Details</h3>
+                     <ul>
+                      <li>name: ${req.body.name}</li>
+                      <li>email : ${req.body.email}</li>
+                     </ul>
+                     <h2>Message</h2>
+                      <p>message: ${req.body.subject}</p>
+                          `;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "funnyboy14143@gmail.com",
+      pass: "wmclufigfkxisyvy"
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+  //Preparing the mailOptions object
+  var mailOptions = {
+    from: "funnyboy14143@gmail.com",
+    to: "funnyboy14143@gmail.com",
+    subject: "New Message",
+    text: req.body.subject,
+    html: emailcontent
+  };
+  //Sending email using transporter function
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
